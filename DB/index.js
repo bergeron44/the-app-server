f8ae05cb168520fcc945e7ef838854c2bc539c90
ron berger <ronberger40@gmail.com>
2022-11-07 08:28:27 +0200

    last chanse with conction

--- DB/index.js
+++ DB/index.js
@@ -9,7 +9,7 @@ app.use(express());
 
 //const mongoUri="mongodb+srv://"+userName+":"+password+"@"+cluster+".xfxcrfk.mongodb.net/?retryWrites=true&w=majority"
 // const mongoUri="mongodb+srv://ronberger:8263867rui@clusterforgame.xfxcrfk.mongodb.net/?retryWrites=true&w=majority"
-const mongoUri = process.env.MONGO_URI;
+const mongoUri = "mongodb+srv://ronberger:8263867rui@clusterforgame.xfxcrfk.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = async function connect() {
   require('../models/UsersTable');
